f203700d5a60ebca59795929ae374e2bbc90b92a
Uma Kadam <90750049+umak1106@users.noreply.github.com>
2022-06-26 13:33:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#Library imports
 import numpy as np
 import streamlit as st
 from keras.models import load_model
@@ -23,13 +22,6 @@ st.sidebar.header('About Me & this project')
 st.sidebar.write('Hi! I am Uma kadam from IIIT Guwahati and Stay healthy won under the category Most Technically difficult Hack in Hackbytes. from'
                  ' 160+ participants hosted on Devpost')
 
-if st.sidebar.button(' Go to Devpost ') :
-    webbrowser.open_new_tab('https://devpost.com/software/stay-healthy-suc1ki')
-
-if st.sidebar.button(' Go to Github ') :
-    webbrowser.open_new_tab('https://github.com/umak1106')
-
-if st.sidebar.button(' Go to Linkedin ') :
-    webbrowser.open_new_tab('https://www.linkedin.com/in/uma-kadam-7885341b0/')
-
-
+st.sidebar.write("[Linkedin](https://www.linkedin.com/in/uma-kadam-7885341b0/)")
+st.sidebar.write("[Github](https://github.com/umak1106)")
+st.sidebar.write("[Devpost](https://devpost.com/software/stay-healthy-suc1ki)")
